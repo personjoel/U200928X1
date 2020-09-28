db960d15b98a781a660fe0c8c9177840480612f4
Joel Persson <jjoelppersson@gmail.com>
2020-09-28 16:27:39 +0200

    created safer route to index.html

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('html'));
+//app.use(express.static('html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
+})
 
 app.listen(process.env.PORT || 3000);
