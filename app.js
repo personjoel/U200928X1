0bc6b03a4a1aba56b3e473d085a435dde7be6886
Joel Persson <jjoelppersson@gmail.com>
2020-09-28 16:35:17 +0200

    created router and path to aboutus and index

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const router = express.Router();
+
 
 //app.use(express.static('html'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + 'index.html'));
 })
+
+app.get('/index', function(req, res) {
+    res.sendFile(path.join(__dirname + 'index.html'));
+})
+
+app.get('/aboutus', function(req, res) {
+    res.sendFile(path.join(__dirname + 'aboutus.html'));
+})
+
+app.use('/', router);
 
 app.listen(process.env.PORT || 3000);
